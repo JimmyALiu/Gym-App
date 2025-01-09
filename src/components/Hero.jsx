3584bff86d6cb3a0a74029307d6e972036032fe1
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-08 23:51:51 -0800

    Styling the Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ export default function Hero() {
   return (
     <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
         <p>IT'S TIME TO GET</p>
-        <h1>Swole<span>normous</span></h1>
-        <p>An app built using JS, React, and Tailwind to help plan your workouts</p>
+        <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Swole<span>normous</span></h1>
+        <p className='text-sm md:text-nase font-light'>An app built using JS, React, and Tailwind to help plan your workouts</p>
     </div>
   )
 }
