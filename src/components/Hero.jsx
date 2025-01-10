2e5c8c3c1717325469b99faa66d03b903fa275f1
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-09 23:03:59 -0800

    Updating styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 export default function Hero() {
-  return (
-    <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
-        <p>IT'S TIME TO GET</p>
-        <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Swole<span>normous</span></h1>
-        <p className='text-sm md:text-nase font-light'>An app built using JS, React, and Tailwind to help plan your workouts</p>
-    </div>
-  )
+	return (
+		<div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center'>
+			<div className='flex flex-col gap-4'>
+				<p>IT'S TIME TO GET</p>
+				<h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Swole<span className='text-blue-400 font-medium'>normous</span></h1>
+			</div>
+			<p className='text-sm md:text-nase font-light'>An app built using JS, React, and Tailwind to help plan your workouts</p>
+		</div>
+	)
 }
