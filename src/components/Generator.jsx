8933b5a6d1dd519fb329477320fb8c1bb05cb76b
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-11 17:18:19 -0800

    Update Generator.jsx

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -26,7 +26,7 @@ export default function Generator() {
 				{Object.keys(WORKOUTS).map((type, typeIndex) => {
 					return (
 						<button className='bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg' key={typeIndex}>
-							<p>{type}</p>
+							<p className='capitalize'>{type.replaceAll('_', ' ')}</p>
 						</button>
 					)
 				})}
