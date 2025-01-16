5ca5c77ef7de3d3da58f9042b654ffe8f0b47b4e
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-15 22:28:18 -0800

    styling the excercise cards

--- src/components/ExcerciseCard.jsx
+++ src/components/ExcerciseCard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export default function ExcerciseCard(props) {
-	const { excercise } = props
+	const { excercise, i } = props
 
 	return (
-		<div>Excercise</div>
+		<div className='p-4 rounded-md flex flex-col gap-4 bg-slate-950 sm:flex-wrap'>
+			<div className='flex flex-col sm:flex-row sm:items-center sm:flex-wrap gap-x-4'>
+				<h4 className='text-3xl hidden sm:inline sm:text-4xl md:text-5xl font-semibold text-slate-400'>0{i + 1}</h4>
+				<h2 className='capitalize whitespace-nowrap truncate max-w-full text-lg sm:text-xl md:text-2xl flex-1 sm:text-center'>{excercise.name.replaceAll('_', ' ')}</h2>
+				<p className='text-sm text-slate-400 capitalize'>{excercise.type}</p>
+			</div>
+		</div>
 	)
 }
--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -3,14 +3,14 @@ import SectionWrapper from './SectionWrapper'
 import ExcerciseCard from './ExcerciseCard'
 
 export default function Workout(props) {
-	const { workout } = props
+	const { workout, i } = props
 
 	return (
 		<SectionWrapper header={"welcome to"} title={['The', 'DANGER', 'zone']}>
 			<div className='flex flex-col gap-4'>
 				{workout.map((excercise, i) => {
 					return (
-						<ExcerciseCard excercise={excercise} key={i} />
+						<ExcerciseCard i={i} excercise={excercise} key={i} />
 					)
 				})}
 			</div>
